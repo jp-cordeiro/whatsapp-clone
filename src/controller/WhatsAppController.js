0c6e32e4d6d74dd4e95cc91cbaabe2222ad13bf5
jp-cordeiro <jpc.araujo@hotmail.com>
2020-01-22 00:41:44 -0300

    add prototype functions for elements

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,7 @@
 class WhatsAppController {
 
     constructor(){
+        this.elementsPrototype()
         this.loadElements();
     }
 
@@ -10,5 +11,55 @@ class WhatsAppController {
         document.querySelectorAll('[id]').forEach( element => {
             this.el[Format.getCamelCase(element.id)] = element;
         })
+    }
+
+    elementsPrototype(){
+        Element.prototype.hide = function(){
+            this.style.display = 'none'
+            return this
+        }
+
+        Element.prototype.show = function(){
+            this.style.display = 'block'
+            return this
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+            return this; 
+        }
+
+        Element.prototype.on = function(events, fn){
+            events.split(' ').forEach( event => {
+                this.addEventListener(event, fn)
+            })
+            return this
+        }
+
+        Element.prototype.css = function(styles){
+            for (let name in styles){
+                this.style[name] = styles[name]
+            }
+            return this
+        }
+
+        Element.prototype.addClass = function(name){
+            this.classList.add(name)
+            return this
+        }
+
+        Element.prototype.removeClass = function(name){
+            this.classList.remove(name)
+            return this
+        }
+
+        Element.prototype.toggleClass = function(name){
+            this.classList.toggle(name)
+            return this
+        }
+
+        Element.prototype.hasClass = function(name){
+            return this.classList.contains(name)
+        }
     }
 }
