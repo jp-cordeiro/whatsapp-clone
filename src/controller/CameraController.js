1ab325a32cba06ab5f243e62514c616806b3cdb0
jp-cordeiro <jpc.araujo@hotmail.com>
2020-02-25 09:38:44 -0300

    camera activation

---
+++ src/controller/CameraController.js
@@ -0,0 +1,15 @@
+class CameraController {
+    constructor(videoEl){
+        this._videoEl = videoEl
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then( stream => {
+            //cria objetos em formato binario
+            this._videoEl.src = URL.createObjectURL(stream)
+            this._videoEl.play()
+        }).catch( err => {
+            console.error(err)
+        })
+    }
+}
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -163,6 +163,8 @@ class WhatsAppController {
             this.el.panelCamera.css({
                 'height': 'calc(100% - 120px)'
             })
+
+            this._camera = new CameraController(this.el.videoCamera)
         })
 
         this.el.btnClosePanelCamera.on('click', e =>{
