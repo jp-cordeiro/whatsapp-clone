12900d871ea550fc714f2e20b35e139c5657eb31
jp-cordeiro <jpc.araujo@hotmail.com>
2020-02-25 15:00:38 -0300

    take picture and draw with canvas

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -113,7 +113,26 @@ export class WhatsAppController {
         })
 
         this.el.btnTakePicture.on('click' , _ => {
+            let dataUrl = this._camera.takePicture()
 
+            this.el.pictureCamera.src = dataUrl
+            this.el.pictureCamera.show()
+            this.el.videoCamera.hide()
+            this.el.btnReshootPanelCamera.show()
+            this.el.containerTakePicture.hide()
+            this.el.containerSendPicture.show()
+        })
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+            this.el.pictureCamera.hide()
+            this.el.videoCamera.show()
+            this.el.btnReshootPanelCamera.hide()
+            this.el.containerTakePicture.show()
+            this.el.containerSendPicture.hide()
+        })
+
+        this.el.btnSendPicture.on('click', e => {
+            console.log(this.el.pictureCamera.src)
         })
 
         //Document
